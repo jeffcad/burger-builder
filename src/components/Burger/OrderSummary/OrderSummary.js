f0e71a7e260e420e07e546bbdec275b8c6f7c8dc
Jeff Cadieux <jeffcadieux@hotmail.com>
2021-01-02 20:39:27 +0900

    Change Modal and OrderSummary to class, lifecycle

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,41 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../../../hoc/Auxiliary'
 import Button from '../../UI/Button/Button'
 
-const OrderSummary = (props) => {
-  let ingredientSummary = []
-  Object.entries(props.ingredients).forEach(entry => {
-    const [ingredientName, ingredientCount] = entry
-    ingredientSummary.push(
-      <li key={ingredientName}>
-        <span style={{ textTransform: 'capitalize' }}>
-          {ingredientName}
-        </span>
-        : {ingredientCount}
-      </li>)
-  })
+class OrderSummary extends Component {
+  componentDidUpdate() {
+    console.log('[OrderSummary] componentDidUpdate')
+  }
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A tasty burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button
-        buttonType="Danger"
-        clicked={props.purchaseCancelled}
-      >CANCEL
+  render() {
+
+    let ingredientSummary = []
+    Object.entries(this.props.ingredients).forEach(entry => {
+      const [ingredientName, ingredientCount] = entry
+      ingredientSummary.push(
+        <li key={ingredientName}>
+          <span style={{ textTransform: 'capitalize' }}>
+            {ingredientName}
+          </span>
+        : {ingredientCount}
+        </li>)
+    })
+
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A tasty burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button
+          buttonType="Danger"
+          clicked={this.props.purchaseCancelled}
+        >CANCEL
       </Button>
-      <Button
-        buttonType="Success"
-        clicked={props.purchaseContinued}
-      >CONTINUE
+        <Button
+          buttonType="Success"
+          clicked={this.props.purchaseContinued}
+        >CONTINUE
       </Button>
-    </Aux>
-  )
+      </Aux>
+    )
+  }
 }
 
 export default OrderSummary
