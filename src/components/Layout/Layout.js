815f1daa38228fde757fd680f899895787cfb1bd
Jeff Cadieux <jeffcadieux@hotmail.com>
2021-01-02 20:17:28 +0900

    Add side drawer navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../../hoc/Auxiliary'
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const Layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-)
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
+
+  sideDrawerToggleHandler = () => {
+    this.setState(prevState => {
+      return { showSideDrawer: !prevState.showSideDrawer }
+    })
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false })
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar
+          drawerToggleClicked={this.sideDrawerToggleHandler}
+        />
+        <SideDrawer
+          isOpen={this.state.showSideDrawer}
+          close={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    )
+  }
+}
 
 export default Layout
