eba0a820df7837a90a179390434a8c236c3fd732
Jeff Cadieux <jeffcadieux@hotmail.com>
2021-01-02 11:37:39 +0900

    Add and remove ingredients

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import classes from './Burger.module.css'
+import Ingredient from './Ingredients/Ingredient'
+
+const Burger = (props) => {
+  // Instructor's solution, it's hard to understand so I made my own below it
+  // const transformedIngredients = Object.keys(props.ingredients)
+  //   .map(igKey => {
+  //     return [...Array(props.ingredients[igKey])].map((_, i) => {
+  //       return <Ingredient key={igKey + i} type={igKey} />
+  //     })
+  //   })
+
+  let middleIngredients = []
+  Object.entries(props.ingredients).forEach(entry => {
+    const [ingredientName, ingredientCount] = entry
+    for (let i = 1; i <= ingredientCount; i++) {
+      middleIngredients.push(<Ingredient key={ingredientName + i} type={ingredientName} />)
+    }
+  })
+
+  if (middleIngredients.length === 0) {
+    middleIngredients = <h4>This is a sad burger. Please add some ingredients!</h4>
+  }
+
+  return (
+    <div className={classes.Burger}>
+      <Ingredient type="bread-top" />
+      {middleIngredients}
+      <Ingredient type="bread-bottom" />
+    </div>
+  )
+}
+
+export default Burger
