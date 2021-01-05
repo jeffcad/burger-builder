4fd312475dc85ac908f7a7a3df72063c1d2b5377
Jeff Cadieux <jeffcadieux@hotmail.com>
2021-01-05 09:32:16 +0900

    Add CheckoutSummary

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import Burger from '../../Burger/Burger'
+import Button from '../../UI/Button/Button'
+import classes from './CheckoutSummary.module.css'
+
+const CheckoutSummary = (props) => {
+  return (
+    <div className={classes.CheckoutSummary}>
+      <h1>It's gonna be great!</h1>
+      <div style={{
+        width: '100%',
+        margin: 'auto'
+      }}>
+        <Burger ingredients={props.ingredients} />
+      </div>
+      <Button buttonType="Danger">CANCEL</Button>
+      <Button buttonType="Success">CONTINUE</Button>
+    </div>
+  )
+}
+
+export default CheckoutSummary
---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
+
+class Checkout extends Component {
+
+  state = {
+    ingredients: {
+      lettuce: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  }
+
+  render() {
+
+    return (
+      <div>
+        <CheckoutSummary ingredients={this.state.ingredients} />
+      </div>
+    )
+  }
+}
+
+export default Checkout
