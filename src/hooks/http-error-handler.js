8782d73b2aa8e3fcc1c70fda1062705633fb945b
Jeff Cadieux <jeffcadieux@hotmail.com>
2021-01-13 15:55:54 +0900

    Fix problem modal not showing on HTTP error

--- src/hooks/http-error-handler.js
+++ src/hooks/http-error-handler.js
@@ -1,29 +1,32 @@
 import { useState, useEffect } from 'react'
 
 export default httpClient => {
-  const [error, setError] = useState(null);
+
+  const [error, setError] = useState(null)
 
   const reqInterceptor = httpClient.interceptors.request.use(req => {
-    setError(null);
-    return req;
-  });
+    setError(null)
+    return req
+  })
+
   const resInterceptor = httpClient.interceptors.response.use(
     res => res,
     err => {
-      setError(err);
+      setError(err)
+      return Promise.reject(err)
     }
-  );
+  )
 
   useEffect(() => {
     return () => {
-      httpClient.interceptors.request.eject(reqInterceptor);
-      httpClient.interceptors.response.eject(resInterceptor);
-    };
-  }, [reqInterceptor, resInterceptor]);
+      httpClient.interceptors.request.eject(reqInterceptor)
+      httpClient.interceptors.response.eject(resInterceptor)
+    }
+  }, [reqInterceptor, resInterceptor])
 
   const errorConfirmedHandler = () => {
     setError(null);
-  };
+  }
 
-  return [error, errorConfirmedHandler];
+  return [error, errorConfirmedHandler]
 }
